3ccccce6b71291957fba19736ba4f9da76b50ec1
Ninomocos <javierclemente.pardo@gmail.com>
2025-01-24 23:56:06 +0100

    04

--- GGJ2025/Assets/Scripts/BubbleMovements/RandomMovement.cs
+++ GGJ2025/Assets/Scripts/BubbleMovements/RandomMovement.cs
@@ -1,6 +1,6 @@
 using UnityEngine;
 
-public class ElipticMove : MonoBehaviour
+public class RandomMovement : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
