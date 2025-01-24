52f2512c71d76a6aba623f5e0d5dd56a122f499a
CrimsonBirdsStudio <crimsonbirdsstudio@gmail.com>
2025-01-24 22:32:22 +0100

    PlayerMovement(unfinished)

--- GGJ2025/Assets/Scripts/Player_Movement.cs
+++ GGJ2025/Assets/Scripts/Player_Movement.cs
@@ -1,16 +1,49 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Player_Movement : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public float vel,stamina,staminaRefill,maxStamina;
+    public Image staminaIndicator;
+
+    Vector2 _targetPos;
+    Rigidbody2D _rb;
+    bool _charging;
     void Start()
     {
-        
+        _rb = GetComponent<Rigidbody2D>();
+        stamina = maxStamina;
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+    }
+    private void FixedUpdate()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            _targetPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            _rb.AddForce(_targetPos * vel);
+            StaminaIndicator(false);
+        }
+        else
+        {
+            StaminaIndicator(true);
+        }
+        staminaIndicator.transform.position = Input.mousePosition;        
+    }
+    void StaminaIndicator(bool up)
+    {
+        int direction = up ? 1 : -2;
+        if (!up && stamina < maxStamina)
+        {
+            stamina += (staminaRefill * direction) * Time.deltaTime;
+        }
+        else
+        {
+            stamina += (staminaRefill * direction) * Time.deltaTime;
+        }
+        stamina = Mathf.Clamp(stamina, 0, maxStamina);
+        staminaIndicator.fillAmount = stamina / maxStamina;
     }
 }
